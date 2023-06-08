fb1d6295c3b303bc8b478105536fea7dac8268f6
Salayo Din <salajo17@gmail.com>
2023-06-08 00:46:18 -0400

    first commit

---
+++ website/app.js
@@ -0,0 +1,8 @@
+/* Global Variables */
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+let baseURL = 'https://openweathermap.org/'
+let apiKey = "ff5aa49c2abcb713e98dff328328fe81";
